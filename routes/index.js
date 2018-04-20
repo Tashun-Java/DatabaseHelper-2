1e21654ceea406c119b829c07b0df634cd8a88fb
Tashun <ttashun.15@cse.mrt.ac.lk>
2018-04-20 19:04:18 +0530

    after update

--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,65 @@
 var express = require('express');
-var FoodItem=require('../models/foodItem');
-var InventoryItem=require('../models/inventoryItem');
+var FoodItem = require('../models/foodItem');
+var InventoryItem = require('../models/inventoryItem');
 var router = express.Router();
-var mongoose=require("mongoose");
+var mongoose = require("mongoose");
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
 //get all data from database
-router.get('/FoodItems',function (req,res,next) {
- FoodItem.find(function (err,FoodItems) {
-   res.send(FoodItems);
- });
+router.get('/FoodItems', function (req, res, next) {
+    FoodItem.find(function (err, FoodItems) {
+        res.send(FoodItems);
+    });
 });
 
 //add a new Item to intentory
-router.post('/addInventory',function (request,response) {
-    var inventoryItem= new InventoryItem({
-        name:request.body.name,
-        stock:request.body.stock
+router.post('/addInventory', function (request, response) {
+    var inventoryItem = new InventoryItem({
+        name: request.body.name,
+        stock: request.body.stock
 
     });
-   inventoryItem.save(function (err) {
-       if (err){
-           throw err;
-       }
-       else {
-           response.send("Data Saved Successfully!");
-       }
-   });
+    inventoryItem.save(function (err) {
+        if (err) {
+            throw err;
+        }
+        else {
+            response.send("Data Saved Successfully!");
+        }
+    });
 });
 
+router.post("/updateInventoryItem", function (req, res, next) {
+    InventoryItem.findOneAndUpdate(
+        {name: req.body.name},
+        {$set: {stock: req.body.stock}},
+        {returnOriginal: false})
+        .then(function (result) {
+            res.send(result);
+        });
+
+
+});
+
+
 // get Inventory
-router.get('/GetInventory',function (req,res,next) {
-    InventoryItem.find(function (err,InventoryItems) {
+router.get('/GetInventory', function (req, res, next) {
+    InventoryItem.find(function (err, InventoryItems) {
         res.send(InventoryItems);
     });
 
 });
 
 //update a item in inventory
-router.get('/updateInventory',function (request,response) {
-    InventoryItem.count( {name:request.body.name}, function (err, count){
-        if (count>0){
-            response.send({validity:true});
+router.get('/updateInventory', function (request, response) {
+    InventoryItem.count({name: request.body.name}, function (err, count) {
+        if (count > 0) {
+            response.send({validity: true});
         }
     });
 
