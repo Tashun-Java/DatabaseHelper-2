6b60091ec94202b03e73b28db8e76be9e3d0e8f2
Tashun <ttashun.15@cse.mrt.ac.lk>
2018-05-16 17:29:58 +0530

    Final

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var FoodItem = require('../models/foodItem');
-var BeverageItem =require('../models/beverageItem');
-var DessertItem=require('../models/dessertItem');
-var JuiceItem=require('../models/juiceItem');
-var KottuItem=require('../models/kottuItem');
-var NoodlesItem=require('../models/noodleItem');
-var RiceAndCurryItem=require('../models/riceAndCurryItem');
-var ShortEatItem=require('../models/shortEatItem');
-var SpecialItem=require('../models/specialItem');
-var TableItem=require('../models/tableItem')
+var BeverageItem = require('../models/beverageItem');
+var DessertItem = require('../models/dessertItem');
+var JuiceItem = require('../models/juiceItem');
+var KottuItem = require('../models/kottuItem');
+var NoodlesItem = require('../models/noodleItem');
+var RiceAndCurryItem = require('../models/riceAndCurryItem');
+var ShortEatItem = require('../models/shortEatItem');
+var SpecialItem = require('../models/specialItem');
+var TableItem = require('../models/tableItem')
 
 var InventoryItem = require('../models/inventoryItem');
 var router = express.Router();
@@ -80,7 +80,7 @@ router.post('/addInventory', function (request, response) {
     var inventoryItem = new InventoryItem({
         name: request.body.name,
         stock: request.body.stock,
-        unit:request.body.unit
+        unit: request.body.unit
 
     });
     inventoryItem.save(function (err) {
@@ -111,7 +111,6 @@ router.post('/addTable', function (request, response) {
 });
 
 
-
 router.post("/updateInventoryItem", function (req, res, next) {
     InventoryItem.findOneAndUpdate(
         {name: req.body.name},
@@ -123,22 +122,33 @@ router.post("/updateInventoryItem", function (req, res, next) {
 
 
 });
-// add a new food Item from Chef app
-router.post('/addFoodItem',function (request,response) {
-    var foodSent=request.body.obj;
-    var foodObj=JSON.parse(foodSent);
-
-        var foodItem = new FoodItem({
-            name: foodObj.name,
-            image:foodObj.image,
-            price:foodObj.price,
-            ingredients: foodObj.ingredients
-
-
-
+//update table
+router.post("/updateTable", function (req, res, next) {
+    TableItem.findOneAndUpdate(
+        {table: req.body.table},
+        {$set: {seat: req.body.seat}},
+        {returnOriginal: false})
+        .then(function (result) {
+            res.send(result);
         });
-        console.log(foodItem);
-   foodItem.save(function (err) {
+
+
+});
+// add a new food Item from Chef app
+router.post('/addFoodItem', function (request, response) {
+    var foodSent = request.body.obj;
+    var foodObj = JSON.parse(foodSent);
+
+    var foodItem = new FoodItem({
+        name: foodObj.name,
+        image: foodObj.image,
+        price: foodObj.price,
+        ingredients: foodObj.ingredients
+
+
+    });
+    console.log(foodItem);
+    foodItem.save(function (err) {
         if (err) {
             throw err;
         }
@@ -149,16 +159,15 @@ router.post('/addFoodItem',function (request,response) {
 
 });
 //add a beverage
-router.post('/addBeverageItem',function (request,response) {
-    var beverageSent=request.body.obj;
-    var beverageObj=JSON.parse(beverageSent);
+router.post('/addBeverageItem', function (request, response) {
+    var beverageSent = request.body.obj;
+    var beverageObj = JSON.parse(beverageSent);
 
     var beverageItem = new BeverageItem({
         name: beverageObj.name,
-        image:beverageObj.image,
-        price:beverageObj.price,
+        image: beverageObj.image,
+        price: beverageObj.price,
         ingredients: beverageObj.ingredients
-
 
 
     });
@@ -175,14 +184,14 @@ router.post('/addBeverageItem',function (request,response) {
 });
 
 //add a dessert
-router.post('/addDessertItem',function (request,response) {
-    var dessertSent=request.body.obj;
-    var dessertObj=JSON.parse(dessertSent);
+router.post('/addDessertItem', function (request, response) {
+    var dessertSent = request.body.obj;
+    var dessertObj = JSON.parse(dessertSent);
 
     var dessertItem = new DessertItem({
         name: dessertObj.name,
-        image:dessertObj.image,
-        price:dessertObj.price,
+        image: dessertObj.image,
+        price: dessertObj.price,
         ingredients: dessertObj.ingredients
 
 
@@ -200,16 +209,15 @@ router.post('/addDessertItem',function (request,response) {
 });
 
 // add a Juice
-router.post('/addJuiceItem',function (request,response) {
-    var juiceSent=request.body.obj;
-    var juiceObj=JSON.parse(juiceSent);
+router.post('/addJuiceItem', function (request, response) {
+    var juiceSent = request.body.obj;
+    var juiceObj = JSON.parse(juiceSent);
 
     var juiceItem = new JuiceItem({
         name: juiceObj.name,
-        image:juiceObj.image,
-        price:juiceObj.price,
+        image: juiceObj.image,
+        price: juiceObj.price,
         ingredients: juiceObj.ingredients
-
 
 
     });
@@ -225,16 +233,15 @@ router.post('/addJuiceItem',function (request,response) {
 
 });
 //add Kottu
-router.post('/addKottuItem',function (request,response) {
-    var kottuSent=request.body.obj;
-    var kottuObj=JSON.parse(kottuSent);
+router.post('/addKottuItem', function (request, response) {
+    var kottuSent = request.body.obj;
+    var kottuObj = JSON.parse(kottuSent);
 
     var kottuItem = new KottuItem({
         name: kottuObj.name,
-        image:kottuObj.image,
-        price:kottuObj.price,
+        image: kottuObj.image,
+        price: kottuObj.price,
         ingredients: kottuObj.ingredients
-
 
 
     });
@@ -250,16 +257,15 @@ router.post('/addKottuItem',function (request,response) {
 
 });
 //add noodles
-router.post('/addNoodlesItem',function (request,response) {
-    var noodlesSent=request.body.obj;
-    var noodlesObj=JSON.parse(noodlesSent);
+router.post('/addNoodlesItem', function (request, response) {
+    var noodlesSent = request.body.obj;
+    var noodlesObj = JSON.parse(noodlesSent);
 
     var noodlesItem = new NoodlesItem({
         name: noodlesObj.name,
-        image:noodlesObj.image,
-        price:noodlesObj.price,
+        image: noodlesObj.image,
+        price: noodlesObj.price,
         ingredients: noodlesObj.ingredients
-
 
 
     });
@@ -276,16 +282,15 @@ router.post('/addNoodlesItem',function (request,response) {
 });
 
 //add rice and curry
-router.post('/addRiceAndCurryItem',function (request,response) {
-    var riceAndCurrySent=request.body.obj;
-    var riceAndCurryObj=JSON.parse(riceAndCurrySent);
+router.post('/addRiceAndCurryItem', function (request, response) {
+    var riceAndCurrySent = request.body.obj;
+    var riceAndCurryObj = JSON.parse(riceAndCurrySent);
 
     var riceAndCurryItem = new RiceAndCurryItem({
         name: riceAndCurryObj.name,
-        image:riceAndCurryObj.image,
-        price:riceAndCurryObj.price,
+        image: riceAndCurryObj.image,
+        price: riceAndCurryObj.price,
         ingredients: riceAndCurryObj.ingredients
-
 
 
     });
@@ -303,16 +308,15 @@ router.post('/addRiceAndCurryItem',function (request,response) {
 
 
 //add a short Eat
-router.post('/addShortEatItem',function (request,response) {
-    var shortEatSent=request.body.obj;
-    var shortEatObj=JSON.parse(shortEatSent);
+router.post('/addShortEatItem', function (request, response) {
+    var shortEatSent = request.body.obj;
+    var shortEatObj = JSON.parse(shortEatSent);
 
     var shortEatItem = new ShortEatItem({
         name: shortEatObj.name,
-        image:shortEatObj.image,
-        price:shortEatObj.price,
+        image: shortEatObj.image,
+        price: shortEatObj.price,
         ingredients: shortEatObj.ingredients
-
 
 
     });
@@ -328,16 +332,15 @@ router.post('/addShortEatItem',function (request,response) {
 
 });
 // add special item
-router.post('/addSpecialItem',function (request,response) {
-    var specialSent=request.body.obj;
-    var specialObj=JSON.parse(specialSent);
+router.post('/addSpecialItem', function (request, response) {
+    var specialSent = request.body.obj;
+    var specialObj = JSON.parse(specialSent);
 
     var specialItem = new SpecialItem({
         name: specialObj.name,
-        image:specialObj.image,
-        price:specialObj.price,
+        image: specialObj.image,
+        price: specialObj.price,
         ingredients: specialObj.ingredients
-
 
 
     });
@@ -369,13 +372,103 @@ router.get('/GetTable', function (req, res, next) {
 
 });
 
-//update a item in inventory
-router.get('/updateInventory', function (request, response) {
-    InventoryItem.count({name: request.body.name}, function (err, count) {
-        if (count > 0) {
-            response.send({validity: true});
+
+function canMadeTheOrder(inventorySent) {
+    return new Promise(function (resolve, reject) {
+
+            InventoryItem.find({name: inventorySent.name}, function (err, inventoryitems) {
+                if (parseFloat(inventoryitems[0].stock) >= parseFloat(inventorySent.stock)) {
+                    // console.log(inventoryitems[0].name);
+                    // console.log(parseFloat(inventoryitems[0].stock));
+                    // console.log(parseFloat(inventorySent.stock));
+                    // console.log("Okey");
+                    return resolve("y");
+
+
+                }
+                else {
+                    console.log("not Okey");
+                    return reject("not Ok");
+
+                }
+
+
+            });
+
+
         }
+    );
+
+
+}
+
+//check if there is that inventory a item in inventory
+router.post('/isInventory', function (request, response) {
+    var res = "";
+    var j = 0;
+    var inventorySent = request.body.obj;
+
+    var chain = Promise.resolve();
+    for (var i = 0; i < inventorySent.length; i++) {
+        chain = chain.then(
+            canMadeTheOrder(inventorySent[i]).then(function (result) {
+                res +=result+",";
+                j++;
+                console.log(res);
+
+                if(j==inventorySent.length)response.send(res);
+            }).catch(function (err) {
+                response.send(err);
+            }));
+    }
+
+
+
+
+});
+
+
+function respond(response, result) {
+    response.send(result);
+
+}
+
+//update the inventory for an order
+router.post("/updateInventoryForAnOrder", function (request, response) {
+
+    InventoryItem.find({name: request.body.name}, function (err, inventory) {
+
+        if (err) {
+            console.log("here");
+            console.log(err);
+        }
+
+
+        if ((Number(inventory[0].stock) - Number(request.body.stock)) > 0) {
+            if (request.body.unit === ("unit")) {
+                var value1 = (Number(inventory[0].stock) - Number(request.body.stock)).toString();
+                console.log(value1 + "If method");
+            }
+            else {
+                var value1 = (Number(inventory[0].stock) - (Number(request.body.stock) / 1000)).toString();
+                console.log("Else Method");
+            }
+
+            InventoryItem.findOneAndUpdate(
+                {name: request.body.name},
+                {$set: {stock: value1}},
+                {returnOriginal: false})
+                .then(function (result) {
+                    response.send(result);
+                });
+        }
+        else {
+            response.send("Unable to Perform")
+        }
+
+
     });
+
 
 });
 
